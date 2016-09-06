a6c88bb103faad93c3eaf1110eb44d159c0d1e8d
Goutham Reddy <gouthamvreddy@gmail.com>
2016-09-06 00:36:35 -0700

    add express development server and configured webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,16 +1,25 @@
-import webpack from 'webpack';
-import path from 'path';
+var webpack = require('webpack');
+var path = require('path');
 
-export default {
-  entry: ['./src/index'],
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: './src/index',
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
     filename: 'bundle.js'
   },
+  devServer: {
+    contentBase: './src'
+  },
   module: {
     loaders: [
-      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']}
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'src'),
+        exclude: /node_modules/,
+        loaders: ['babel']
+      }
     ]
   }
 }
