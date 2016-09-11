158133f3dd14bac61dc8faa303bdcc5f703adf46
Goutham Reddy <gouthamvreddy@gmail.com>
2016-09-11 15:58:33 -0700

    updated webpack config file for css modules

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,7 +1,7 @@
-var webpack = require('webpack');
-var path = require('path');
+import webpack from 'webpack';
+import path from 'path';
 
-module.exports = {
+export default {
   devtool: 'cheap-module-eval-source-map',
   entry: './src/index',
   output: {
@@ -14,12 +14,8 @@ module.exports = {
   },
   module: {
     loaders: [
-      {
-        test: /\.js$/,
-        include: path.join(__dirname, 'src'),
-        exclude: /node_modules/,
-        loaders: ['babel']
-      }
+      {test: /\.js$/, include: path.join(__dirname, 'src'), exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ["style-loader", "css-loader?modules&importLoaders=1&localIdentName=[name]_[local]_[hash:base64:5]"]},
     ]
   }
-}
+};
