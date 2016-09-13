2a3bc968b5d1dd5bd6f850a04a8fed4a2d40c2f4
Goutham Reddy <gouthamvreddy@gmail.com>
2016-09-13 15:14:40 -0700

    added Typewriter animation text to Home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
 import styles from './Home.css';
 import ScrollDown from '../ScrollDown/ScrollDown';
+import TypeWriter from 'react-typewriter';
 
 export default class Main extends Component {
   render() {
     return (
-      <section className={styles.home}>
-        <div className={styles.homeOverlay} />
-        <ScrollDown />
+      <section style={{height: "100%"}}>
+        <section className={styles.home} >
+          <div className={styles.homeOverlay} />
+          <TypeWriter typing={1} minDelay={35}>
+            <span style={{fontSize: '3em', position: 'absolute', top: "10px", left: "10px"}}>
+              > Hello world!<br />
+              > I'm Goutham Reddy<br />
+              > Welcome to my personal website
+            </span>
+          </TypeWriter>
+          <ScrollDown />
+        </section>
       </section>
     );
   }
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -18,7 +18,8 @@ export default {
       {test: /\.js$/, include: path.join(__dirname, 'src'), exclude: /node_modules/, loaders: ['babel']},
       {test: /\.css$/, loader: "style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]_[local]_[hash:base64:5]!postcss-loader"},
       {test: /\.png(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=100000&mimetype=image/png"},
-      {test: /\.jpg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?mimetype=image/jpg"}
+      {test: /\.jpg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?mimetype=image/jpg"},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
     ]
   },
   postcss: function () {
