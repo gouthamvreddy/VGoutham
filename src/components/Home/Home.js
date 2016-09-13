7e832993f7700766b98f7de5c4a92aa9c5854a52
Goutham Reddy <gouthamvreddy@gmail.com>
2016-09-12 22:12:39 -0700

    added ScrollDown button to Home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import styles from './Home.css';
+import ScrollDown from '../ScrollDown/ScrollDown';
 
 export default class Main extends Component {
   render() {
     return (
       <section className={styles.home}>
         <div className={styles.homeOverlay} />
+        <ScrollDown />
       </section>
     );
   }
