00e2ad19177eca7e9d29f50fce7fda1ae9603982
Goutham Reddy <gouthamvreddy@gmail.com>
2016-10-01 00:43:18 -0700

    Replaced react-typewriter with react-typist and added react-typist styles to index.html

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import styles from './Home.css';
 import AnimatedChevronButton from '../AnimatedChevronButton/AnimatedChevronButton';
-import TypeWriter from 'react-typewriter';
+import Typist from 'react-typist';
 import Scroll from 'react-scroll';
 import About from '../About/About';
 import Footer from '../Footer/Footer';
@@ -19,14 +19,12 @@ export default class Main extends Component {
       <section style={{height: "100%"}}>
         <section className={styles.home} >
           <div className={styles.homeOverlay} />
-          <TypeWriter typing={1} minDelay={35}>
-            <span style={{fontSize: '3em', position: 'absolute', top: "10px", left: "10px"}}>
+          <Typist className={styles.typist}>
               > Hello world!<br />
               > I&apos;m Goutham Reddy<br />
               > Welcome to my personal website
-            </span>
-          </TypeWriter>
-          <Link className="About" to="About" smooth={1} duration={500} ><AnimatedChevronButton /></Link>
+          </Typist>
+          <Link className="About" to="About" smooth={Boolean(true)} duration={500} ><AnimatedChevronButton /></Link>
         </section>
         <Element name="About" className="element">
           <About />
