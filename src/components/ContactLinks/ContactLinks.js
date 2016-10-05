66c22e361fb9271c331026175c932d2eab54a566
Goutham Reddy <gouthamvreddy@gmail.com>
2016-10-04 22:21:48 -0700

    Updated spacing to make social media link cubes to be more responsive

--- src/components/ContactLinks/ContactLinks.js
+++ src/components/ContactLinks/ContactLinks.js
@@ -6,22 +6,22 @@ import styles from './ContactLinks.css';
 const ContactLinks = () => {
     return (
       <Row>
-        <Col className={styles.contactCol} xsOffset={2} xs={2} sm={2} smOffset={2} md={1} mdOffset={4} lg={1} lgOffset={4}>
+        <Col className={styles.contactCol} xsOffset={0} xs={6} sm={3} smOffset={0} md={2} mdOffset={2} lg={2} lgOffset={2}>
           <section className={styles.contactLinks}>
             <a href="https://plus.google.com/u/0/+GouthamReddy5" className={styles.icon + ' ' + styles.iconCube + ' ' + styles.googleplus}>Google</a>
           </section>
         </Col>
-        <Col className={styles.contactCol} xs={2} sm={2} md={1} lg={1}>
+        <Col className={styles.contactCol} xs={6} sm={3} md={2} lg={2}>
           <section className={styles.contactLinks}>
             <a href="http://twitter.com/9outham" className={styles.icon + ' ' + styles.iconCube + ' ' + styles.twitter}>Twitter</a>
           </section>
         </Col>
-        <Col className={styles.contactCol} xs={2} sm={2} md={1} lg={1}>
+        <Col className={styles.contactCol} xs={6} sm={3} md={2} lg={2}>
           <section className={styles.contactLinks}>
             <a href="https://github.com/gouthamvreddy" className={styles.icon + ' ' + styles.iconCube + ' ' + styles.github}>Github</a>
           </section>
         </Col>
-        <Col className={styles.contactCol} xs={2} sm={2} md={1} lg={1}>
+        <Col className={styles.contactCol} xs={6} sm={3} md={2} lg={2}>
           <section className={styles.contactLinks}>
             <a href="https://facebook.com/goreddy" className={styles.icon + ' ' + styles.iconCube + ' ' + styles.facebook}>Facebook</a>
           </section>
